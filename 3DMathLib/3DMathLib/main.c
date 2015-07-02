9f99c7211ea38ed01935ad6272a1f68aa609b664
liutianshx2012 <liutianshx@163.com>
2015-07-02 10:41:31 +0800

    //3dMathLib

---
+++ 3DMathLib/3DMathLib/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  3DMathLib
+//
+//  Created by TTc on 15/7/2.
+//  Copyright (c) 2015年 TTc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
